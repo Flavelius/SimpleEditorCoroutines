7c5bd8c6a22f42dd1cb01ef358b22cabca3aef2e
ValooFX <mail@heijulien.de>
2017-01-25 10:49:56 +0100

    Upload

---
+++ Assets/SimpleEditorCoroutines/Editor/EditorCoroutines.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+// ReSharper disable once CheckNamespace
+namespace SimpleEditorCoroutines
+{
+
+    public enum EditorCoroutineState { Running, Done, Canceled }
+
+    public class EditorCoroutine
+    {
+        readonly IEnumerator routineReference;
+        readonly Action<EditorCoroutine> cancel;
+
+        /// <summary>
+        /// Use this to start the Editor coroutine.
+        /// </summary>
+        /// <returns>Returns an object that can be used to check the status of or cancel the routine</returns>
+        public static EditorCoroutine Start(IEnumerator routine)
+        {
+            return EditorCoroutines.StartRoutine(routine);
+        }
+
+        EditorCoroutine(IEnumerator routine, Action<EditorCoroutine> cancelAction)
+        {
+            routineReference = routine;
+            cancel = cancelAction;
+        }
+
+        public EditorCoroutineState State { get; private set; }
+
+        public void Cancel()
+        {
+            cancel(this);
+        }
+
+        static class EditorCoroutines
+        {
+            static readonly List<EditorCoroutine> activeRoutines = new List<EditorCoroutine>();
+
+            static void UpdateActiveRoutines()
+            {
+                for (var i = activeRoutines.Count; i-- > 0;)
+                {
+                    if (!activeRoutines[i].routineReference.MoveNext())
+                    {
+                        activeRoutines[i].State = EditorCoroutineState.Done;
+                        activeRoutines.RemoveAt(i);
+                    }
+                }
+                if (activeRoutines.Count > 0)
+                {
+                    EditorApplication.delayCall += UpdateActiveRoutines;
+                }
+            }
+
+            public static EditorCoroutine StartRoutine(IEnumerator routine)
+            {
+                var ec = new EditorCoroutine(routine, CancelRoutine);
+                if (activeRoutines.Count == 0) EditorApplication.delayCall += UpdateActiveRoutines;
+                activeRoutines.Add(ec);
+                ec.State = EditorCoroutineState.Running;
+                return ec;
+            }
+
+            static void CancelRoutine(EditorCoroutine routine)
+            {
+                routine.State = EditorCoroutineState.Canceled;
+                activeRoutines.Remove(routine);
+            }
+        }
+
+}
+}
---
+++ Assets/SimpleEditorCoroutines/Editor/Test/EditorCoroutineTest.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace SimpleEditorCoroutines.Test
+{
+    public class EditorCoroutineTest : EditorWindow
+    {
+
+        [MenuItem("Window/EditorCoroutine Test")]
+        static void Open()
+        {
+            GetWindow<EditorCoroutineTest>("CoroutineTest");
+        }
+
+        List<PrimeTest> Tests = new List<PrimeTest>()
+        {
+            new PrimeTest(5000)
+        };
+
+        void OnGUI()
+        {
+            for (var i = 0; i < Tests.Count; i++)
+            {
+                Tests[i].DrawGUI();
+            }
+            if (GUILayout.Button("+"))
+            {
+                Tests.Add(new PrimeTest(10000));
+            }
+        }
+
+        class PrimeTest
+        {
+            public PrimeTest(int maxPrimes)
+            {
+                numPrimes = maxPrimes;
+            }
+
+            int numPrimes;
+            EditorCoroutine runningRoutine;
+
+            public void DrawGUI()
+            {
+                EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+                EditorGUILayout.LabelField("Calculate prime numbers");
+                numPrimes = EditorGUILayout.IntField("Stop After", numPrimes);
+                numPrimes = Mathf.Clamp(numPrimes, 100, 100000);
+                if (runningRoutine == null || runningRoutine.State == EditorCoroutineState.Canceled || runningRoutine.State == EditorCoroutineState.Done)
+                {
+                    if (GUILayout.Button("Start"))
+                    {
+                        runningRoutine = EditorCoroutine.Start(FindPrimeNumber(numPrimes));
+                    }
+                }
+                else
+                {
+                    if (runningRoutine != null && GUILayout.Button("Cancel"))
+                    {
+                        runningRoutine.Cancel();
+                    }
+                }
+                if (runningRoutine != null)
+                {
+                    var prevColor = GUI.backgroundColor;
+                    switch (runningRoutine.State)
+                    {
+                            case EditorCoroutineState.Running:
+                            GUI.backgroundColor = Color.yellow;
+                            break;
+                        case EditorCoroutineState.Canceled:
+                            GUI.backgroundColor = Color.red;
+                            break;
+                            case EditorCoroutineState.Done:
+                            GUI.backgroundColor = Color.green;
+                            break;
+                    }
+                    EditorGUILayout.LabelField(runningRoutine.State.ToString(), EditorStyles.helpBox);
+                    GUI.backgroundColor = prevColor;
+                }
+                EditorGUILayout.EndVertical();
+            }
+        }
+
+        void Update()
+        {
+            Repaint();
+        }   
+
+        //borrowed from StackExchange
+        static IEnumerator FindPrimeNumber(int n)
+        {
+            var count = 0;
+            long a = 2;
+            while (count < n)
+            {
+                long b = 2;
+                var prime = 1;
+                while (b * b <= a)
+                {
+                    if (a % b == 0)
+                    {
+                        prime = 0;
+                        break;
+                    }
+                    b++;
+                }
+                if (prime > 0)
+                {
+                    count++;
+                }
+                a++;
+                if (count % 1000 == 0)
+                {
+                    yield return null;
+                }
+            }
+        }
+    }
+}
